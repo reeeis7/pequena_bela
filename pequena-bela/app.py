dff9cfd964f1f114e73540d9d40394e088069819
pedroclopes3@gmail.com <pedroclopes3@gmail.com>
2024-10-01 08:35:53 -0300

    att

---
+++ pequena-bela/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/produtos')
+def about():
+    return render_template('produtos.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
