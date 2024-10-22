001cf49a8a6419520680b32a4fcac9c70b8d0fab
pedroclopes3@gmail.com <pedroclopes3@gmail.com>
2024-10-22 13:41:47 -0300

    att

--- pythonstrings/endereco.py
+++
@@ -1,9 +0,0 @@
-nome = input("Qual seu nome e sobrenome? ")
-cidade = input("Qual sua cidade? ")
-estado = input("Qual seu estado? ")
-rua = input("Digite o nome da rua da sua casa ")
-numero = input("Digite o número da sua casa ")
-bairro = input("Digite o nome do seu bairro ")
-
-print(f'Prezado(a) {nome.capitalize()}')
-print(f'Sua residência fica em {rua.capitalize()}, {numero}, {bairro.capitalize()}, {cidade.capitalize()} - {estado.upper()}')
